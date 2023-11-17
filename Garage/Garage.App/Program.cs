9a65e7c136707d7a5d723ef4daae19931f81a885
LLarndorfer <116261839+LLarndorfer@users.noreply.github.com>
2023-11-17 11:02:27 +0100

    Add files via upload

---
+++ Garage/Garage.App/Program.cs
@@ -0,0 +1,80 @@
+﻿
+using System.Linq.Expressions;
+using System.Security.Permissions;
+using GarageLogic;
+
+var garage = new Garage();
+
+// var ihsgih = garage.ParkingSpots[7].licensePlate;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+string? input = null;
+string? licenseplateinput = null;
+DateTime userentertime;
+DateTime userexittime;
+int userparkingslot = 0;
+
+do
+{
+    System.Console.WriteLine("What do you want to do?");
+    System.Console.WriteLine("1) Enter a car entry");
+    System.Console.WriteLine("2) Enter a car exit");
+    System.Console.WriteLine("3) Generate a report");
+    System.Console.Write("4) Exit \nYour selection: ");
+    input = Console.ReadLine()!;
+
+    switch (input)
+    {
+        case "1":
+            System.Console.Write("Enter parking spot number: ");
+            userparkingslot = int.Parse(Console.ReadLine()!) - 1;
+            if (!garage.IsOccupied(userparkingslot))
+            {
+                System.Console.Write("Enter license plate: ");
+                licenseplateinput = Console.ReadLine()!;
+                System.Console.Write("Enter entry/date time:");
+                userentertime = DateTime.Parse(Console.ReadLine()!);
+                garage.ParkingSpots[userparkingslot] = new()
+                {
+                    entryDate = userentertime,
+                    licensePlate = licenseplateinput
+                };
+            }
+            else
+            {
+                System.Console.WriteLine("Parking spot is occupied");
+            }
+            break;
+
+        case "2":
+            System.Console.Write("Enter parking spot number: ");
+            userparkingslot = int.Parse(Console.ReadLine()!) - 1;
+            System.Console.Write("Enter exit date/time: ");
+            userexittime = DateTime.Parse(Console.ReadLine()!);
+            if (garage.TryExit(userparkingslot, userexittime, out var costs))
+            {
+                System.Console.WriteLine($"Costs are {costs}€");
+            }
+            else
+            {
+                System.Console.WriteLine("Parking spot is not occupied!");
+            }
+            break;
+
+        case "3":
+            var text = garage.GenerateReport();
+            
+            foreach (var t in text)
+            {
+                System.Console.WriteLine(t);
+            }
+            break;
+
+        case "4":
+            System.Console.WriteLine("Goodbye!");
+            break;
+        default:
+            throw new ArgumentException("Invalid type of Input.");
+    }
+} while (input != "4");
+
---
+++ Garage/Garage.Logic/Class1.cs
@@ -0,0 +1,97 @@
+﻿namespace GarageLogic;
+
+public class ParkingSpot
+{
+    public string? licensePlate { get; set; }
+
+    public DateTime entryDate { get; set; }
+}
+
+public class Garage
+{
+    public ParkingSpot[] ParkingSpots { get; } = new ParkingSpot[50];
+
+    public bool IsOccupied(int parkingSpotNumber)
+    {
+        if (ParkingSpots[parkingSpotNumber] != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool TryOccupy(int parkingSpotNumber, string licensePlate, DateTime entryTime)
+    {
+        if (ParkingSpots[parkingSpotNumber] == null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    public bool TryExit(int parkingSpotNumber, DateTime exitTime, out decimal costs)
+    {
+        if (IsOccupied(parkingSpotNumber))
+        {
+            if ((exitTime.Subtract(ParkingSpots[parkingSpotNumber].entryDate).TotalMinutes) > 15)
+            {
+                costs = (decimal)Math.Ceiling((exitTime.Subtract(ParkingSpots[parkingSpotNumber].entryDate).TotalMinutes) / 15) * 3;
+            }
+            else
+            {
+                costs = 0;
+            }
+            ParkingSpots[parkingSpotNumber] = null;
+            return true;
+
+        }
+        else
+        {
+            costs = 0;
+            return false;
+        }
+
+    }
+
+    public string[] GenerateReport()
+    {
+        string[] text = new string[52];
+        text[0] = "| Spot | License Plate |";
+        text[1] = "| ---- | ------------- |";
+        for (int i = 0; i < ParkingSpots.Length; i++)
+        {
+            if (ParkingSpots[i] != null)
+            {
+                if (i < 9)
+                {
+                    text[i + 2] = $"|  {i + 1}   | {ParkingSpots[i].licensePlate}";
+                }
+                else
+                {
+                    text[i + 2] = $"|  {i + 1}  | {ParkingSpots[i].licensePlate}";
+                }
+            }
+
+            else
+            {
+                if (i < 9)
+                {
+                    text[i + 2] = $"|  {i + 1}   | - |";
+                }
+                else
+                {
+                    text[i + 2] = $"|  {i + 1}  | - |";
+                }
+            }
+        }
+
+        return text;
+    }
+}
